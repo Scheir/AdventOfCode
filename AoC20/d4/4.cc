3f2614d401dc00baaf9c654a4dee9f985ed7ecb9
Andre Scheir Johansson <41080829+Scheir@users.noreply.github.com>
2021-06-16 17:32:53 +0200

    Update 4.cc

--- AoC20/d4/4.cc
+++ AoC20/d4/4.cc
@@ -99,13 +99,11 @@ int main(){
                 string tstr = str.substr(pos, space-pos);
                 if(!x.second(tstr)){
                     ok2 = false;
-                    break;
                 }
             }
             else{
                 ok = false;
                 ok2 = false;
-                break;
             }
         }
         if(ok){
