ca88574c47d91274a5ed13f9d17d6ad76ebb9f12
Andre Scheir Johansson <41080829+Scheir@users.noreply.github.com>
2022-12-07 07:18:32 +0100

    Update 7.py

--- 2022/d7/7.py
+++ 2022/d7/7.py
@@ -14,18 +14,17 @@ for row in arr:
         elif x[2] == "/":
             stk = ["/"]
         else:
-            stk.append(x[2])
-
-    # These two arent needed in my solution
-    elif x[1] == "ls" or x[0] == "dir":
-        continue
-
-    else:
+            stk.append(x[ 2 ])
+            
+    elif x[0].isnumeric():
         # Add the size to current and all parents:
         # stk = [/,a,b,c]
         # /, /a, /a/b, /a/b/c -> all are increased
         for i in range(len(stk)):
             D['/'.join(stk[:i+1])] += int(x[0])
+    else:
+        pass 
+        # Useless data
 
 print(sum(v for v in D.values() if v <= 100000))
 
